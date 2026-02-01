dd20134b375f319e8c3649788d040c9b9a6fb0b6
Sarah Binney <Sarah.Binney@softwire.com>
2026-02-01 16:30:42 +0000

    add footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,26 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 relative">
-      <div className="absolute inset-0 bg-[url(/images/tile-grid-black.png)] bg-size-[17px] opacity-20 bg-position-[0_1]" />
+    <footer className="bg-secondary-200">
       <div className="container relative">
-        <div className="flex flex-col items-center py-28 lg:flex-row">
-          <h3 className="mb-10 text-center text-4xl font-mono leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-2xl">
-            Built with Sanity + Next.js.
-          </h3>
-          <div className="flex flex-col gap-3 items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-            <a
-              href="https://github.com/sanity-io/sanity-template-nextjs-clean"
-              className="rounded-full flex gap-2 font-mono whitespace-nowrap items-center bg-black hover:bg-blue focus:bg-blue py-3 px-6 text-white transition-colors duration-200"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on GitHub
-            </a>
-            <a href="https://nextjs.org/docs" className="mx-3 hover:underline font-mono">
-              Read Next.js Documentation
-            </a>
-          </div>
+        <div className="flex flex-row items-center pt-12 lg:flex-row gap-6 justify-center text-sm sm:text-base leading-6 tracking-tight">
+          <Link href="https://twitter.com/DreamcatPuzzles" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </Link>
+          |
+          <Link
+            href="https://www.instagram.com/dreamcat.puzzles"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </Link>
+          |
+          <Link href="https://mastodon.me.uk/@dreamcat" target="_blank" rel="noopener noreferrer">
+            Mastodon
+          </Link>
+          |
+          <Link
+            href="https://buttondown.email/DreamcatPuzzles"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Newsletter
+          </Link>
         </div>
+        <div className="flex justify-center pb-10">
+        <Link
+            href="https://terpeca.com/2025/"
+            target="_blank"
+            rel="noopener noreferrer"
+            ><Image
+              src="/images/2025TERPECABadgeNominee.svg"
+              alt="2025 TERPECA Award Nominee Badge"
+              width={300}
+              height={400}
+              className="p-4"
+          /></Link>
+          </div>
       </div>
     </footer>
   )
